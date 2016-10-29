2f5f296ca190ce8e76b2f494612639ee1c454013
Lawrence Pan <lawrencefeipan@gmail.com>
2016-10-29 17:41:05 -0400

    Create LICENSE

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clojagar "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Agar.io clone in Clojure and ClojureScript"
+  :url "https://github.com/lpan/clojagar"
+  :license {:name "MIT License"
+            :url "https://github.com/lpan/clojagar/blob/master/LICENSE"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.89" :scope "provided"]
